14b1f25d45c22247af8af98c919ef523e78fba35
JMorenoPicon <jmorenopicon@adaits.es>
2025-03-08 20:38:52 +0100

    added jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,16 @@
 export default {
     testEnvironment: 'node',
     verbose: true,
+    collectCoverage: true, // Habilitar la recopilación de cobertura
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
     transform: {
-        '^.+\\.js$': 'babel-jest'
-    }
+        '^.+\\.js$': 'babel-jest',  // Transforma los archivos .js usando babel-jest
+    },
 };
