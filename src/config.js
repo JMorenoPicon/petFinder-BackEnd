cc9fd3618ef40243452e5f52884c9ac29aa02266
JMorenoPicon <jmorenopicon@adaits.es>
2025-03-10 03:25:31 +0100

    security config and validations roles added

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ const config = {
     level: process.env.NODE_ENV === 'production' ? 'error' : 'info', // Ajustar el nivel de logs
 
     security: {
-        JWT_SECRET: process.env.JWT_SECRET, // Definición de la clave JWT (a implementar más adelante)
+        JWT_SECRET: process.env.JWT_SECRET, // Definición de la clave JWT desde .env
     },
 
     mongoUri: process.env.MONGO_URI, // URI para la conexión a la base de datos
